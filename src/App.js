f71c1e86efb46cb5f52c07834c6011ba859c494a
Your Name <you@example.com>
2023-12-12 10:50:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react'
 
-function App() {
+const App = () => {
+  
+  const [city,setCity] = useState("");
+  const [result,setResult] = useState("");
+  const changeHandler = e =>{
+    setCity(e.target.value);
+  }
+  const submitHandler = e =>{
+    e.preventDefault();
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=d885aa1d783fd13a55050afeef620fcb`).then(
+      response=> response.json()).then(
+        data => {
+          const kelvin = data.main.temp;
+          const celcius = kelvin - 273.15;
+          setResult("Temperature at "+city+"\n"+Math.round(celcius)+"°C");
+        }
+      ).catch(error => console.log(error))
+      setCity("");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{margin:"20px"}}>
+      <center>
+        <div className="card" style={{backgroundImage:"linear-gradient(lightblue,darkblue)", width:"400px",height:"460px",borderRadius:"25px", padding:"8px"}}>
+          <div className="card-body">
+            <h4 className="card-title" style={{colo:"white",fontSize:"20px"}}>Weather App</h4>
+            <form onSubmit={submitHandler} >
+              <input style={{borderRadius:"25px" ,height:"30px"}} placeholder="Enter City" size="30" type="text" name="city" onChange={changeHandler} value={city}/> <br /><br />
+              <input style={{borderRadius:"25px" ,color:""}} type="submit" value="Get Temperature" />
+            </form><br /> <br />
+            <div>
+              <img src="https://img.freepik.com/premium-vector/cloudy-sun-cloud-cute-weather-realistic-icon-3d-cartoon_363543-491.jpg?w=740" width="150px" heigth="150px"  style={{borderRadius:"50%"}}/> 
+               <h1 style={{color:"white"}}>{result}</h1> 
+            </div>
+          </div>
+        </div>
+      </center>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
